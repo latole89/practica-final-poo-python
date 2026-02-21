e69d6f2687154ef62fd2212f9810f490ea7d8473
latole89 <latole89@gmail.com>
2026-02-20 21:37:04 -0300

    Creamos un sistema en Python para dar una solución a Patas Felices a través de una APP de consola.

---
+++ clases/cliente.py
@@ -0,0 +1,20 @@
+class Cliente:
+    def __init__(self, nombre, direccion, telefono):
+        self.nombre = nombre
+        self.direccion = direccion
+        self.telefono = telefono
+        self.historial_compras = []
+    
+    def actualizar_infotmacion(self, direccion = None, telefono = None):
+        if direccion:
+            self.direccion = direccion
+        if telefono:
+            self.telefono = telefono
+
+    def registrar_compra(self,compra):
+        self.historial_compras.append(compra)
+
+    def mostrar_informacion(self):
+        return f"Cliente: {self.nombre}, Dirección: {self.direccion}, Telêfono: {self.telefono}"
+    
+
